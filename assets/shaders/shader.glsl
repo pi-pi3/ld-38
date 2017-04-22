e901d6d91153698c9bda74c1c6c2101ef6145bec
pi_pi3 <walter.szymon.98@gmail.com>
2017-04-22 09:45:32 +0200

    Update shader.glsl

--- assets/shaders/shader.glsl
+++ assets/shaders/shader.glsl
@@ -17,25 +17,8 @@ vec4 position(mat4 _, vec4 vertex) {
 
 #ifdef PIXEL
 
-//uniform vec4 u_light;
-const float exposure = 1.5;
-const float gamma = 2.2;
-
 vec4 effect(vec4 _col, Image s_color, vec2 _uv, vec2 _sc) {
-    // temporary
-    vec4 u_light = vec4(0.0, 0.0, -1.0, 1.0);
-
-    vec4 color  = gammaToLinear(_col);
-    vec3 normal = normalize(f_normal);
-    float shade = max(0.0, dot(normal, u_light.xyz));
-    color.rgb *= shade;
-    color.rgb *= u_light.w;
-
-    // RomBinDaHouse
-    color.rgb = exp( -1.0 / ( 2.72*color.rgb + 0.15 ) );
-    color.rgb = pow(color.rgb, vec3(1.0 / gamma));
-
-    return vec4(color.rgb, 1.0);
+    return vec4(_col.rgb, 1.0);
 }
 
 #endif
